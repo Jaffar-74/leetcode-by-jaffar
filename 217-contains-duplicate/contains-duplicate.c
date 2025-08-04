ecdf0ad41aea7e171935c0583318535ef75f997a
Mohammed Jaffar Sadiq S M <119559862+Jaffar-74@users.noreply.github.com>
2025-08-04 20:30:43 +0530

    Time: 48 ms (43.14%) | Memory: 17.3 MB (55.44%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.c
@@ -0,0 +1,18 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+int cmp(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+bool containsDuplicate(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), cmp);  // Sort the array
+
+    for (int i = 0; i < numsSize - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            return true;
+        }
+    }
+
+    return false;
+}
